5463a5139abae0f9500e86ae0cef5d13abbe8d88
sergio urrego <sergiourrego2016@gmail.com>
2023-07-13 15:43:45 -0500

    hoy 13 de julio

--- php/update/updatecliente_admin.php
+++ php/update/updatecliente_admin.php
@@ -4,12 +4,15 @@ require_once('../conecxion.php');
 date_default_timezone_set('America/Bogota');
 $fecha_actual = date("Y-m-d h:i:s");
 
-$actualizar="UPDATE clientes c 
+$actualizar="UPDATE c 
 set c.cedula='$_POST[cedula]', c.nombre='$_POST[nombre]', 
 c.apellidos='$_POST[apellidos]', c.correo='$_POST[correo]', 
 c.celular='$_POST[celular]', c.usuario_actualizo='$_SESSION[user]',
-c.fecha_actualizo='$fecha_actual' WHERE c.nit='$_SESSION[select_empresa]' ";
+c.fecha_actualizo='$fecha_actual' from detalles_clientes dc inner join clientes c on c.cedula=.cd.cedula_cliente WHERE dc.nit='$_SESSION[select_empresa]' ";
 $resultado=mysqli_query($con,$actualizar);
-
+echo $actualizar;
+if ($resultado){
+    echo "entro";
+}
 
 ?>
